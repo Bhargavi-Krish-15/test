a5e3b7ec695732f6f6c6ac4b7ebce10d44bf056a
Bhargavi-Krish-15 <95071003+Bhargavi-Krish-15@users.noreply.github.com>
2022-02-22 12:23:23 +0530

    Update test6.rb

--- test6.rb
+++ test6.rb
@@ -1,18 +1,21 @@
 array = [-1, 2, -3, 4, 5, 6, -7, 8, 9]
 pos = []
-neg = []
+neg =[]
 new = []
 array.each do |n|
   if n > 0
-    pos.push(n)
+    pos << n
   else
-    neg.push(n)
+    neg << n
   end
 end
 pos = pos.reverse
 neg = neg.reverse
-
-pos.each_index do |i|
-  print "#{pos[i]} "
-  print "#{neg[i]} "
+pos.each_index do |n|
+  new << pos[n]
+  if n < neg.length
+    new << neg[n]
+  end
 end
+
+p new
